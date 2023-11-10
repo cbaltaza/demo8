f4040bca684888de31eb3e6dd6fdc5eeba9a4b5b
cbaltaza <balta1_carlos@hotmail.com>
2023-11-09 21:18:23 -0600

    Update ClienteImpl.java

--- src/main/java/com/example/demoj8/ClienteImpl.java
+++ src/main/java/com/example/demoj8/ClienteImpl.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -25,17 +24,22 @@ public class ClienteImpl implements Cliente {
 	
 	@Override
 	public void consultarDatos(String id) {
-		String uRLf = "https://api.belvo.com/api/invoices/?link={id}";
+		String uRLf = "https://sandbox.belvo.com/api/invoices/?link={id}";
 		String uRLAux = uRLf.replace("{id}", id);
 		   String userAndPass = "33e035a2-4d13-467c-ab7c-0f7a3d1605cc:5yGpoWU87*njiK7_cObWozHNv465lbFqPhdxBww4b5#O_t0LF7Oou7L@Mtpi*Lht";
+			log.info("Datos uRLAux {} ", uRLAux);
+			String pwdf = Base64.getEncoder().encodeToString(userAndPass.getBytes());
+			log.info("Datos pwdf {}", pwdf);
 
+			 
 		HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_XML);
-	    headers.add("Authorization", "Basic " +  Base64.getEncoder().encodeToString(userAndPass.getBytes()));
-	    
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    headers.add("Authorization", "Basic " + pwdf);
+		log.info("Datos headers {}", headers);
+
 		ResponseEntity<String> salida = restTemplate.exchange(uRLAux, HttpMethod.GET, new HttpEntity<Object>(headers), String .class);
 		
-		log.info("Datos de Salida del metodo post para Guardar en base de datos", salida);
+		log.info("Datos de Salida del metodo post para Guardar en base de datos {}", salida.getBody());
 		
 	}
 	
