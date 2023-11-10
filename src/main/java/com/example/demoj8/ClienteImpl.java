2e3158fb2ced8d70eb4844ec7eab00f313bbe1b0
cbaltaza <balta1_carlos@hotmail.com>
2023-11-09 20:29:51 -0600

    Update ClienteImpl.java
    3863 service

--- src/main/java/com/example/demoj8/ClienteImpl.java
+++ src/main/java/com/example/demoj8/ClienteImpl.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class ClienteImpl implements Cliente {
 
 	 private static final Logger log = LoggerFactory.getLogger(WebHookController.class);
